35e928df394321072cc24f9d36dd288236676c6d
suey0128 <syui0128@gmail.com>
2021-07-01 01:17:03 -0700

    commit tree d3bc3d296292554a8a1bb8df99075bcf48e7b1b5

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
 class Cat
-  # code goes here
+  attr_accessor  :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize name,owner, mood="nervous" #owner is an owner instance 
+    @name = name
+    @owner = owner
+    @mood = mood
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,76 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize name, species="human"
+    @name = name
+    @species = species
+    @@all << self
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def cats
+    #go to the all cat arr and find those with self.name
+     Cat.all.select{|cat| cat.owner === self}
+  end
+
+  def dogs
+    #go to the all dog arr and find those with self.name
+    Dog.all.select{|dog| dog.owner === self}
+  end
+
+  def buy_cat name #take in string
+    #create a cat instance and associate it with self
+    Cat.new(name,self)
+  end
+
+  def buy_dog name
+    Dog.new(name,self)
+  end
+
+  def walk_dogs
+    #access the dog mood and change it 
+    self.dogs.each{|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each{|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    self.cats.each{|cat| 
+      #make them nervous
+      cat.mood = "nervous"
+      #leave them without the owner
+      cat.owner = nil
+    }
+    self.dogs.each{|dog| 
+      dog.mood = "nervous"
+      dog.owner = nil
+    }
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
+
 end
+
+# @owner = Owner.new("Victoria")
